c8fc790cb186e3b2cab5775ae0bfba1ba88023ef
Adam Shortsleeves <ashortsleeves@gmail.com>
2017-03-06 18:11:05 +0000

    0.1.1 Imported CSS and Javascript, ie files and images are missing

---
+++ app/assets/javascripts/scrollbar.js
@@ -0,0 +1,10 @@
+$("html").niceScroll({
+    mousescrollstep: 70,
+    cursorcolor: "#e44c65",
+    cursorwidth: "5px",
+    cursorborderradius: "10px",
+    cursorborder: "black",
+    horizrailenabled: false,
+    mousescrollstep: 20,
+    zindex: 999999,
+});
