4362ce4dec8ad838f33286bb6be9efe58e2c9c75
Adam Shortsleeves <ashortsleeves@gmail.com>
2017-03-10 19:15:00 +0000

    0.2.5 added pagination

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'devise'
 gem 'cancancan', '~> 1.10'
 
 gem "font-awesome-rails"
+
+gem 'will_paginate', '~> 3.1.0'
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
     belongs_to :user, foreign_key: 'user_id'
     validates :body, presence: true
     validates :user, presence: true
+    self.per_page = 2
 end
